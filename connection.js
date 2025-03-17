1ce91e9d98ba341521b172569a0ae422892a53df
anil gade <143864724+anilgadhe@users.noreply.github.com>
2025-03-17 21:20:33 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose")
 
 async function connectDb (url){
 
-    return await mongoose.connect(url);
+   mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // Lower timeout for faster failover
+  socketTimeoutMS: 45000, // Keep socket alive
+  keepAlive: true
+});
+
 
 }
 
